3233d0794239caaf4980e65d25fc13effb093dc9
akash.kava <ackava@gmail.com>
2015-08-06 18:39:21 +0530

    OData query tried

--- NuGetProxy/Controllers/HomeController.cs
+++ NuGetProxy/Controllers/HomeController.cs
@@ -69,7 +69,9 @@ namespace NuGetProxy.Controllers
                         id = id.Trim('\'');
                         version = version.Trim('\'');
 
-                        builder.Path = "/api/v2/package/" + id + "/" + version;
+                        //builder.Path = "/api/v2/package/" + id + "/" + version;
+
+                        builder.Path = string.Format("/api/v2/Packages()?$filter=(Id%20eq%20%27{0}%27)and(Version%20eq%27{1}%27)",id,version);
 
                         //throw new Exception("Redirecting to " + builder.ToString());
                     }
